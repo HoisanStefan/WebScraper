969714470364082c52d93d00fc8a292e91e63719
HoisanStefan <candul12345@gmail.com>
2023-09-30 16:42:53 +0300

    scraping contents

--- services/scrapeService.js
+++ services/scrapeService.js
@@ -1,43 +1,95 @@
-const axios = require("axios");
 const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
+
+const getHtml = async (url) => {
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+
+  await page.goto(url);
+
+  await page.waitForTimeout(3000);
+
+  // Get the fully-rendered HTML
+  const html = await page.content();
+
+  await browser.close();
+
+  return html;
+};
 
 const scrapeURL = async (url) => {
-  let ans = await axios(url)
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-      const articles = [];
-      const scripts = [];
+  const html = await getHtml(url);
+  const $ = cheerio.load(html);
 
-      $("div").each((index, element) => {
-        const article = {};
+  const texts = [];
+  const anchors = [];
+  const scripts = [];
+  const images = [];
 
-        article.title = $(element).find("div").text();
-
-        articles.push(article);
-      });
-
-      $("script").each((index, element) => {
-        let script = {};
-
-        const text = $(element).text();
-
-        script = { ...element.attribs, type: element.name };
-
-        if (text) {
-          script = { ...script, text };
-        }
-        scripts.push(script);
-      });
-
-      return { success: true, articles, scripts };
-    })
-    .catch((error) => {
-      console.error(error.message);
-      return { success: false, data: [] };
+  //   Texts
+  function extractText(element) {
+    const children = $(element).contents();
+    children.each((index, child) => {
+      if (child.type === "text" && $(child).text().trim() != "") {
+        texts.push($(child).text().trim());
+      } else if (child.type === "tag") {
+        extractText(child);
+      }
     });
+  }
 
-  return ans;
+  extractText("body");
+
+  //   Links
+  $("a").each((index, element) => {
+    const text = $(element).text();
+    let link = $(element).attr("href");
+
+    if (text && text != "" && link && link != "") {
+      if (!link.startsWith("http")) {
+        link = url + link;
+      }
+
+      anchors.push({ text, link: link });
+    }
+  });
+
+  //   Scripts
+  $("script").each((index, element) => {
+    let src = $(element).attr("src");
+
+    if (src) {
+      if (!src.startsWith("http")) {
+        src = url + src;
+      }
+      scripts.push(src);
+    }
+  });
+
+  //   Images
+  $("img").each((index, element) => {
+    let src = $(element).attr("src");
+
+    if (src) {
+      if (!src.startsWith("http")) {
+        src = url + src;
+      }
+      images.push(src);
+    }
+  });
+
+  return {
+    success: true,
+    texts,
+    anchors,
+    scripts,
+    images,
+    numberOfTexts: texts.length,
+    numberOfAnchors: anchors.length,
+    numberOfScripts: scripts.length,
+    numberOfImages: images.length,
+  };
 };
 
 module.exports = {
